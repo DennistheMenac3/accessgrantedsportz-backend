a8304a827106afd6bbcd10a50bc8fe1c4cffaee8
Dennis Grant <dennis.grant91@gmail.com>
2026-04-21 21:21:04 -0700

    added cors access to index files

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,15 @@ app.post(
 // =============================================
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',               // Let your local dev machine in
+    'https://accessgrantedsportz.com',     // Let your live website in
+    'https://www.accessgrantedsportz.com'  // Let the 'www' version in just in case
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(morgan('dev'));
 
